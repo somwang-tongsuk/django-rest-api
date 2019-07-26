e9c9704d0c71794824de8763f888709146e91fa1
somwangthongsuk <somwangthongsuk@gmail.com>
2019-07-26 09:13:44 +0700

    set product list view.py

--- django-rest-framework/store/catalog/urls.py
+++ django-rest-framework/store/catalog/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-from catalog import views
+from . import views
 
 urlpatterns = [
-    url(r'^products/$', views.ProductList.as_view()),
-    url(r'^products/(?P<product_id>[0-9]+)/$', views.ProductDetail.as_view()),
+    url(r'products/$', views.ProductList.as_view()),
+    url(r'products/(?P<product_id>[0-9]+)/$', views.ProductDetail.as_view()),
 ]
--- django-rest-framework/store/catalog/views.py
+++ django-rest-framework/store/catalog/views.py
@@ -3,10 +3,18 @@ from .models import Product
 from .serializers import ProductSerializer
 
 class ProductList(generics.ListCreateAPIView):
-    queryset = Product.objects.all()
+    #queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_url_kwarg = 'product_id'
 
+    def get_queryset(self):
+        """ allow rest api to filter by name """
+        queryset = Product.objects.all()
+        name = self.request.query_params.get('name', None)
+        if name is not None:
+            queryset = queryset.filter(name=name)
+        
+        return queryset
 
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
